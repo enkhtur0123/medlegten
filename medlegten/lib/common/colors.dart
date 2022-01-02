8b6d1199bbb7132481cacb2537cc24f91acdf014
Bulgan <bulgan@ddishtv.mn>
2022-01-02 15:51:46 +0800

    2022.01.02

--- medlegten/lib/common/colors.dart
+++ medlegten/lib/common/colors.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+const colorPrimary = Color.fromRGBO(48, 53, 159, 1);
+const colorSecondary = Color.fromRGBO(120, 100, 254, 1);
+const colorBlack = Colors.black;
+const colorWhite = Colors.white;
+
+const TextTheme TEXT_THEME_DEFAULT = TextTheme(
+  headline1: TextStyle(
+    color: colorPrimary, fontWeight: FontWeight.w400, fontSize: 16, fontFamily: 'Roboto'
+  ),
+    headline2: TextStyle(
+    color: colorPrimary, fontWeight: FontWeight.w700, fontSize: 18, fontFamily: 'Roboto'
+),
+    headline3: TextStyle(
+        color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 14, fontFamily: 'Roboto', textBaseline: TextBaseline.alphabetic,
+    )
+);
+
 class ColorTable {
   ColorTable._();
 
--- medlegten/lib/pages/StartPages/age.dart
+++ medlegten/lib/pages/StartPages/age.dart
@@ -1,44 +1,100 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:medlegten/common/colors.dart';
+import 'package:medlegten/common/widget_functions.dart';
 import 'package:medlegten/components/wide_button.dart';
+import 'dart:ui';
+import 'package:medlegten/custom/BorderBox.dart';
 
-class AgePage extends StatelessWidget {
+class AgePage extends StatefulWidget {
   const AgePage({Key? key}) : super(key: key);
-  //      await LoginRepository().fetchLoginInfo(fbuser);
+  @override
+  _AgePageState createState() => _AgePageState();
+}
+
+class _AgePageState extends State<AgePage> {
+  DateTime selectedDate = DateTime.now();
 
   @override
+  initState() {
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
+    double screenWidth = window.physicalSize.width;
+    final double paddingTop = 80.0;
+    final double paddingRight = 30.0;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Flexible(
-            child: Text(
-              'Та төрсөн он сар өдрөө оруулна уу! ---..',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: ColorTable.color48_53_159,
-                  fontFamily: 'Roboto-Bold',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  height: 1),
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/img/background_age.png'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          Flexible(
-            flex: 2,
-            child: WideButton(
-              "Continue",
-              ColorTable.color120_100_254,
-              ColorTable.color255_255_255,
-              () {
-                //AutoRouter.of(context).push(const LoginRoute());
-              },
-            ),
-          ),
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: paddingTop, right: paddingRight),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    addHorizontalSpace(10),
+                    Text('алгасах', style: TEXT_THEME_DEFAULT.headline1,)
+                  ],
+                ),
+              ),
+              addVerticalSpace(80),
+              Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Text('Та төрсөн он сар өдрөө оруулна уу', style: TEXT_THEME_DEFAULT.headline2,),
+              ),
+              addVerticalSpace(10),
+              Container(
+                height: 52,
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: TextField(
+                  style: TextStyle(color: colorPrimary),
+                  decoration: InputDecoration(
+                    hintText: 'Он сар өдөр',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: colorPrimary, width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: colorPrimary, width: 1),
+                    ),
+                    suffixIcon: const Icon(Icons.date_range_outlined, color:  colorPrimary,),
+                    filled: true,
+                    fillColor: colorWhite
+                  ),
+                ),
+              ),
+              addVerticalSpace(250),
+              WideButton('Үргэлжлүүлэх', colorSecondary, colorWhite, () {}),
+              addVerticalSpace(20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+
+                child: Text(
+                  'Таны оруулсан хувийн мэдээлэл нь цаашид зөвхөн таны сургалтын хөтөлбөрт зориулагдан ашиглагдах болно',
+                  style: TEXT_THEME_DEFAULT.headline3, textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          )
         ],
-      ),
+      )
     );
   }
 }
+
+
+
