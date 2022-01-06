15b835ea368ea8049517ae52f2df79007d9c6a9e
Bulgan <bulgan@ddishtv.mn>
2022-01-06 15:42:35 +0800

    2022.01.06 bulgaa

--- medlegten/lib/pages/CoursePages/course_cart.dart
+++ medlegten/lib/pages/CoursePages/course_cart.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:medlegten/common/colors.dart';
+import 'package:medlegten/common/widget_functions.dart';
 import 'package:medlegten/models/Landing/course_info.dart';
+import "package:intl/intl.dart";
+
+
 
 class CourseCart extends StatelessWidget {
   const CourseCart(this.courseInfo, {Key? key}) : super(key: key);
@@ -9,13 +13,13 @@ class CourseCart extends StatelessWidget {
 
   final TextStyle textStyle = const TextStyle(color: Colors.black);
 
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: SizedBox(
           width: double.infinity,
-          height: 120,
           child: Card(
             color: ColorTable.color255_255_255,
             shadowColor: Colors.grey.withOpacity(0.5),
@@ -28,16 +32,64 @@ class CourseCart extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                      // Text('data', style: TextStyle(color: Colors.red),),
-                      Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/img/Course/course_bg_a1.png'),
-                            fit: BoxFit.cover,
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(courseInfo.courseName, style: TextStyle(color: colorPrimary, fontFamily: 'Roboto', fontSize: 16, fontWeight: FontWeight.w400),),
+                              ),
+                              addVerticalSpace(5),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(courseInfo.shortDesc, style: TextStyle(color: Color.fromRGBO(189, 189, 189, 1), fontFamily: 'Roboto', fontSize: 10, fontWeight: FontWeight.w400),),
+                              ),
+                              addVerticalSpace(5),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    child: Icon(
+                                      Icons.person_outline_outlined,
+                                      color: Color.fromRGBO(130, 130, 130, 1),
+                                      size: 12.0,
+                                    )
+                                  ),
+                                  addHorizontalSpace(3),
+                                  Text(courseInfo.levelName, style: TextStyle(color: Color.fromRGBO(130, 130, 130, 1), fontWeight: FontWeight.w400, fontSize: 12)),
+                                  addHorizontalSpace(20),
+                                  SizedBox(
+                                      child: Icon(
+                                        Icons.timer,
+                                        color: Color.fromRGBO(130, 130, 130, 1),
+                                        size: 12.0,
+                                      )
+                                  ),
+                                  addHorizontalSpace(3),
+                                  Text(courseInfo.hours+' цаг', style: TextStyle(color: Color.fromRGBO(130, 130, 130, 1), fontWeight: FontWeight.w400, fontSize: 12)),
+                                  addHorizontalSpace(20),
+                                  SizedBox(
+                                      child: Icon(
+                                        Icons.feed_outlined,
+                                        color: Color.fromRGBO(130, 130, 130, 1),
+                                        size: 12.0,
+                                      )
+                                  ),
+                                  addHorizontalSpace(3),
+                                  Text(courseInfo.units+' хичээл', style: TextStyle(color: Color.fromRGBO(130, 130, 130, 1), fontWeight: FontWeight.w400, fontSize: 12)),
+                                ],
+                              ),
+                              addVerticalSpace(5),
+                              Text(toMoney(courseInfo.price), style: TextStyle(color: colorPrimary, fontWeight: FontWeight.w700, fontSize: 23, fontFamily: 'Roboto')),
+
+                            ],
                           ),
                         ),
-                        child: null /* add child content here */,
                       ),
+                      courseBgImg(courseInfo.courseId, courseInfo.levelName),
                   ],
                 )
               ],
@@ -46,4 +98,45 @@ class CourseCart extends StatelessWidget {
         ),
     );
   }
+}
+
+toMoney(stringMoney) {
+  double d = double.parse(stringMoney);
+  final currencyFormatter = NumberFormat('#,##0', 'MNG');
+  return currencyFormatter.format(d)+'₮';
+}
+
+courseBgImg(courseId, courseLevel) {
+  var imgUrl;
+
+  switch(courseId) {
+    case "1": {  imgUrl = 'assets/img/Course/course_bg_a1.png'; }
+    break;
+    case "2": {  imgUrl = 'assets/img/Course/course_bg_a2.png'; }
+    break;
+  }
+
+  return Container(
+      height: 120, width: 112,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(imgUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: [
+          addVerticalSpace(30),
+          Text(courseLevel+' level'.toUpperCase(), style: TextStyle(color: colorWhite, fontWeight: FontWeight.w700, fontFamily: 'Roboto', fontSize: 16),),
+          addVerticalSpace(10),
+          OutlinedButton(
+              onPressed: null,
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(width: 1.0, color: colorWhite),
+              ),
+              child: Text('Buy now', style: TextStyle(color: colorWhite, fontWeight: FontWeight.w500, fontSize: 14, fontFamily: 'Roboto'),)
+          ),
+        ],
+      )
+  );
 }
