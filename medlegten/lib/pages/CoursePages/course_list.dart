06cb7289204da4277e5306e36f10570af334c068
DESKTOP-CCTBBLC\lenovo <enkhtur0123@gmail.com>
2022-01-16 09:58:53 +0800

    commit at 2022 01 16

--- medlegten/lib/pages/CoursePages/course_list.dart
+++ medlegten/lib/pages/CoursePages/course_list.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:medlegten/common/colors.dart';
