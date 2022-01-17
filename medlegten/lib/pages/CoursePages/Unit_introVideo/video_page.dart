f7c191748fc1e2a8edc41d71671a05cc48dff77e
Bulgan <bulgan@ddishtv.mn>
2022-01-17 16:23:03 +0800

    2021.11.17

--- medlegten/lib/pages/CoursePages/Unit_introVideo/video_page.dart
+++ medlegten/lib/pages/CoursePages/Unit_introVideo/video_page.dart
@@ -20,15 +20,17 @@ class CourseUnitIntroVideoPage extends StatefulWidget {
 
 class _CourseUnitIntroVideoState extends State<CourseUnitIntroVideoPage> {
   late VideoPlayerController _videoPlayerController;
+  bool _showModal = false;
 
   @override
   void initState() {
-    super.initState();
+
     _videoPlayerController =
         VideoPlayerController.network(widget.unitIntroVideo.url)
           ..initialize().then((_) {
             setState(() {});
           });
+    super.initState();
   }
 
   @override
@@ -57,127 +59,32 @@ class _CourseUnitIntroVideoState extends State<CourseUnitIntroVideoPage> {
               top: 0, left: 0, right: 0,
               child: LandingHeader(100),
             ),
+            Positioned(
+                top: 100, left: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if(_showModal)
+                      {
+                        _showModal=false;
+                        setState(() {
+
+                        });
+                      }
+                    else
+                      {
+                        _showModal=true;
+                        setState(() {
+
+                        });
+                      }
+                  },
+                  child: Text("Show Bottom", style: TextStyle(color: colorBlack),),
+                )
+            ),
           ],
         ),
       ),
-      bottomSheet: BottomSheet(
-        backgroundColor: colorWhite,
-        builder: (BuildContext context) {
-          return Container(
-            height: 250,
-            width: double.infinity,
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 20,
-                  left: MediaQuery.of(context).size.width*0.06,
-                  child: Container(
-                    height: 220,
-                    width: MediaQuery.of(context).size.width*0.88,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Color.fromRGBO(174, 177, 239, .3), width: 1),
-
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              SizedBox(
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        color: Color.fromRGBO(120, 100, 254, 1),
-                                        borderRadius: BorderRadius.circular(8),
-                                        border: Border.all(color: Color.fromRGBO(174, 177, 239, .3), width: 1),
-                                      ),
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(left: 12, top: 8, right: 12, bottom: 8),
-                                        child: Text('dancing', style: TextStyle(color: colorWhite, fontWeight: FontWeight.w700, fontSize: 16),),
-                                      ),
-                                    ),
-                                    IconButton(
-                                        onPressed: () => {  },
-                                        icon: const Icon(
-                                          Icons.volume_up_outlined,
-                                          color: Color.fromRGBO(48, 53, 159, 1),
-                                          size: 28.0,
-                                        )
-                                    )
-                                  ],
-                                ),
-                              ),
-                              SizedBox(
-                                child: IconButton(
-                                    onPressed: () => {  },
-                                    icon: const Icon(
-                                      Icons.bookmark_outline_sharp,
-                                      color: Color.fromRGBO(48, 53, 159, 1),
-                                      size: 28.0,
-                                    )
-                                ),
-                              )
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Text('Үгийн үндэс', style: TextStyle(color: Color.fromRGBO(48, 53, 159, 1), fontSize: 14, fontWeight: FontWeight.w500),),
-                                addHorizontalSpace(20),
-                                Text('dance', style: TextStyle(color: Color.fromRGBO(48, 53, 159, .3), fontSize: 14, fontWeight: FontWeight.w500),),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Noun', style: TextStyle(color: Color.fromRGBO(48, 53, 159, 1), fontSize: 14, fontWeight: FontWeight.w500),),
-                                Text('Бүжиг, бүжгийн ая, бүжгийн урлаг, бүжгийн цуглаан', style: TextStyle(color: Color.fromRGBO(48, 53, 159, .3), fontSize: 14, fontWeight: FontWeight.w500),),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Verb', style: TextStyle(color: Color.fromRGBO(48, 53, 159, 1), fontSize: 14, fontWeight: FontWeight.w500),),
-                                Text('Бүжиглэх, бүжиглүүлэх, бүжих, гэдгдэх, гялалзах (нүд), гал хөгжих, агаагрт хурдан дээш доош хөдлөх', style: TextStyle(color: Color.fromRGBO(48, 53, 159, .3), fontSize: 14, fontWeight: FontWeight.w500),),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top:                        9.8, left: 40,
-                  child: Container(
-                    child: Image.network(
-                      "https://www.medlegten.com/static/uploads/content/image_small/polygon3.jpg",
-                      fit: BoxFit.cover,
-                      width: 27.56, height: 11.48,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-        onClosing: () {  },
-      )
+      bottomSheet: _showBottomSheet(context),
     );
   }
 
@@ -189,5 +96,134 @@ class _CourseUnitIntroVideoState extends State<CourseUnitIntroVideoPage> {
       seconds: int.parse(splitted[2].split('.')[0]),
       milliseconds: int.parse(splitted[2].split('.')[1]),
     );
+  }
+
+  Widget _showBottomSheet(context) {
+    if(_showModal) {
+        return BottomSheet(
+          backgroundColor: colorWhite,
+          builder: (BuildContext context) {
+            return Container(
+              height: 250,
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 20,
+                    left: MediaQuery.of(context).size.width*0.06,
+                    child: Container(
+                      height: 220,
+                      width: MediaQuery.of(context).size.width*0.88,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Color.fromRGBO(174, 177, 239, .3), width: 1),
+
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SizedBox(
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          color: Color.fromRGBO(120, 100, 254, 1),
+                                          borderRadius: BorderRadius.circular(8),
+                                          border: Border.all(color: Color.fromRGBO(174, 177, 239, .3), width: 1),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(left: 12, top: 8, right: 12, bottom: 8),
+                                          child: Text('dancing', style: TextStyle(color: colorWhite, fontWeight: FontWeight.w700, fontSize: 16),),
+                                        ),
+                                      ),
+                                      IconButton(
+                                          onPressed: () => {  },
+                                          icon: const Icon(
+                                            Icons.volume_up_outlined,
+                                            color: Color.fromRGBO(48, 53, 159, 1),
+                                            size: 28.0,
+                                          )
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  child: IconButton(
+                                      onPressed: () => {  },
+                                      icon: const Icon(
+                                        Icons.bookmark_outline_sharp,
+                                        color: Color.fromRGBO(48, 53, 159, 1),
+                                        size: 28.0,
+                                      )
+                                  ),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Text('Үгийн үндэс', style: TextStyle(color: Color.fromRGBO(48, 53, 159, 1), fontSize: 14, fontWeight: FontWeight.w500),),
+                                  addHorizontalSpace(20),
+                                  Text('dance', style: TextStyle(color: Color.fromRGBO(48, 53, 159, .3), fontSize: 14, fontWeight: FontWeight.w500),),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Noun', style: TextStyle(color: Color.fromRGBO(48, 53, 159, 1), fontSize: 14, fontWeight: FontWeight.w500),),
+                                  Text('Бүжиг, бүжгийн ая, бүжгийн урлаг, бүжгийн цуглаан', style: TextStyle(color: Color.fromRGBO(48, 53, 159, .3), fontSize: 14, fontWeight: FontWeight.w500),),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Verb', style: TextStyle(color: Color.fromRGBO(48, 53, 159, 1), fontSize: 14, fontWeight: FontWeight.w500),),
+                                  Text('Бүжиглэх, бүжиглүүлэх, бүжих, гэдгдэх, гялалзах (нүд), гал хөгжих, агаагрт хурдан дээш доош хөдлөх', style: TextStyle(color: Color.fromRGBO(48, 53, 159, .3), fontSize: 14, fontWeight: FontWeight.w500),),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top:                        9.8, left: 40,
+                    child: Container(
+                      child: Image.network(
+                        "https://www.medlegten.com/static/uploads/content/image_small/polygon3.jpg",
+                        fit: BoxFit.cover,
+                        width: 27.56, height: 11.48,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          onClosing: () {  },
+        );
+    }
+    else
+      {
+        return Container(
+          child: Text(''),
+        );
+      }
   }
 }
