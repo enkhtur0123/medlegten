8cac30b7d5b51551bfc4385607d4ed2861d306e0
DESKTOP-CCTBBLC\lenovo <enkhtur0123@gmail.com>
2022-02-11 18:59:55 +0800

    commit at 2022 02 11

--- lib/pages/LandingPages/landing_page.dart
+++ lib/pages/LandingPages/landing_page.dart
@@ -65,19 +65,16 @@ class LandingPageState extends ConsumerState<LandingPage>
               text1: "Өнөөдрийн хичээлдээ бэлэн үү? ✍️"));
         }
         if (tabController!.index == 2) {
-          ref
-              .read(appbarProvider.notifier)
-              .changeStatus(AppBarState(height: 110, isRichText: false,text1: "Have fun!"));
+          ref.read(appbarProvider.notifier).changeStatus(
+              AppBarState(height: 110, isRichText: false, text1: "Have fun!"));
         }
         if (tabController!.index == 3) {
-          ref
-              .read(appbarProvider.notifier)
-              .changeStatus(AppBarState(height: 110, isRichText: false, text1: "Have fun!"));
+          ref.read(appbarProvider.notifier).changeStatus(
+              AppBarState(height: 110, isRichText: false, text1: "Have fun!"));
         }
         if (tabController!.index == 4) {
-          ref
-              .read(appbarProvider.notifier)
-              .changeStatus(AppBarState(height: 110, isRichText: false, text1: "Have fun!"));
+          ref.read(appbarProvider.notifier).changeStatus(
+              AppBarState(height: 110, isRichText: false, text1: "Have fun!"));
         }
       }
     });
